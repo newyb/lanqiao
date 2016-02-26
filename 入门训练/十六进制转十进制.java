fb6f8b6d8af4c6ce2d9561b145aecf905006e80c
newyb <newyb@qq.com>
2016-02-26 22:24:19 +0800

    打卡

--- 入门训练/十六进制转十进制.java
+++ 入门训练/十六进制转十进制.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String n = sc.next();
-		System.out.print(Long.parseLong(n, 16));
+		System.out.print(Long.parseLong(n, 16));//省事的方法
 	}
 }
--- 入门训练/字母图形.java
+++ 入门训练/字母图形.java
@@ -7,7 +7,7 @@ public class Main {
 		int m = sc.nextInt();
 		for (int a = 0; a < n; a++) {
 			for (int b = 0; b < m; b++) {
-				char zm = (char) ('A' + Math.abs(a - b));
+				char zm = (char) ('A' + Math.abs(a - b));//规律
 				System.out.print(zm);
 			}
 			System.out.println();
