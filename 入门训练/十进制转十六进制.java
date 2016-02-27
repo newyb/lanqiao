b1701386a391d3ec28420dd9eebea2a58b536038
newyb <newyb@qq.com>
2016-02-27 21:12:59 +0800

    打卡

--- 入门训练/十进制转十六进制.java
+++ 入门训练/十进制转十六进制.java
@@ -4,6 +4,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-	System.out.printf(Integer.toHexString(n).toUpperCase());
+	System.out.printf(Integer.toHexString(n).toUpperCase());//题目要求是大写
 	}
 }
