0e108a53a86a246d087b7e97512ac5701542adcf
newyb <newyb@qq.com>
2016-03-06 21:29:00 +0800

    改名

---
+++ 基础练习/时间转换.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ 
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        System.out.printf("%d:%d:%d",t/3600,t%3600/60,t%60);
+    }
+}
